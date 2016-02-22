f80054fc57fc8fae1a061f8cd7fe2fbd79d197b0
JacAlexJohns <jjohns37@gustavus.edu>
2016-02-22 10:43:21 -0600

    Updated App with Cheat Button Revised [ver 3] Stops Cheating by Swapping the Cheat Screen [revised] and Adds Animation to the Show Answer Button

--- app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,11 +1,15 @@
 package com.bignerdranch.android.geoquiz;
 
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.animation.Animator;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -55,10 +59,30 @@ public class CheatActivity extends AppCompatActivity {
                 }
                 mCheated = true;
                 setAnswerShownResult(mCheated);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswer.getWidth() / 2;
+                    int cy = mShowAnswer.getHeight() / 2;
+                    float radius = mShowAnswer.getWidth();
+                    Animator anim = ViewAnimationUtils.createCircularReveal(mShowAnswer, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            mAnswerTextView.setVisibility(View.VISIBLE);
+                            mShowAnswer.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    anim.start();
+                } else {
+                    mAnswerTextView.setVisibility(View.VISIBLE);
+                    mShowAnswer.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
         if (savedInstanceState != null) {
+            mCheated = true;
             setAnswerShownResult(savedInstanceState.getBoolean(KEY_CHEAT));
         }
     }
